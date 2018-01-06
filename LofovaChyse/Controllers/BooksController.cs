0b2092de5c39aaea515d68bf695ca66829fe4851
Matěj Štágl <stagl.matej@centrum.cz>
2018-01-06 01:43:55 +0100

    Přidána editace příspěvků

--- LofovaChyse/Controllers/BooksController.cs
+++ LofovaChyse/Controllers/BooksController.cs
@@ -29,9 +29,11 @@ namespace LofovaChyse.Controllers
             return View(books); // Passnu třídu
         }
 
-        public ActionResult Detail(int? id, bool zobrazPopis)
+        public ActionResult Detail(int id, bool zobrazPopis)
         {
-            Book b = (from Book book in Books.GetFakeList where book.Id == id select book).FirstOrDefault();
+            BookDao bookDao = new BookDao();
+            Book b = bookDao.GetbyId(id);
+
             ViewBag.Zobraz = zobrazPopis;
 
             return View(b);
@@ -39,11 +41,15 @@ namespace LofovaChyse.Controllers
 
         public ActionResult Create()
         {
+            BookCategoryDao bookCategoryDao = new BookCategoryDao();
+            IList<BookCategory> categories = bookCategoryDao.GetAll();
+            ViewBag.Categories = categories;
+
             return View();
         }
 
         [HttpPost] // post only
-        public ActionResult Add(Book book, HttpPostedFileBase picture)
+        public ActionResult Add(Book book, HttpPostedFileBase picture, int categoryId)
         {
             if (ModelState.IsValid)
             {
@@ -80,7 +86,13 @@ namespace LofovaChyse.Controllers
                     }
                 }
 
-                Books.GetFakeList.Add(b);
+                BookCategoryDao bookCategoryDao = new BookCategoryDao();
+                BookCategory bookCategory = bookCategoryDao.GetbyId(categoryId);
+
+                b.Category = bookCategory;
+
+                BookDao bookDao = new BookDao();
+                bookDao.Create(b);
 
                 // Notifikace
                 TempData["scs"] = "V pořádku";
@@ -91,6 +103,60 @@ namespace LofovaChyse.Controllers
             }
 
             return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            BookDao bookDao = new BookDao();
+            BookCategoryDao bookCategoryDao = new BookCategoryDao();
+
+            Book b = bookDao.GetbyId(id);
+            ViewBag.Categories = bookCategoryDao.GetAll();
+
+            return View(b);
+        }
+
+        public ActionResult Update(Book book, HttpPostedFileBase picture, int categoryId)
+        {
+            try
+            {
+                BookDao bookDao = new BookDao();
+                BookCategoryDao bookCategoryDao = new BookCategoryDao();
+
+                BookCategory bookCategory = bookCategoryDao.GetbyId(categoryId);
+
+                book.Category = bookCategory;
+
+                if (picture != null)
+                {
+                    Image image = Image.FromStream(picture.InputStream);
+                    Image smalImage = ImageHelper.ScaleImage(image, 200, 200);
+
+                    Bitmap btmBitmap = new Bitmap(smalImage);
+                    Guid guid = Guid.NewGuid();
+
+                    string imageName = guid.ToString() + ".jpg";
+                    btmBitmap.Save(Server.MapPath("~/Uploads/Book/") + imageName, ImageFormat.Jpeg); // Je potřeba namapovat cestu!
+
+                    btmBitmap.Dispose();
+                    image.Dispose();
+
+                    System.IO.File.Delete(Server.MapPath("~/Uploads/Book/") + book.ImageName);
+
+                    book.ImageName = imageName;
+                }
+
+                bookDao.Update(book);
+
+                TempData["scs"] = "Kniha " + book.Name + " byla upravena";
+            }
+            catch (Exception e)
+            {
+               
+                throw;
+            }
+
+            return RedirectToAction("Index", "Books");
         }
     }
 }
