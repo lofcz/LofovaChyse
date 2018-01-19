574ae2b6d14a2f12168b7fe1bd381d97c879e80a
Matěj Štágl <stagl.matej@centrum.cz>
2018-01-19 19:22:18 +0100

    Implementace profilů

---
+++ LofovaChyse/Controllers/ProfileController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataAccess.Dao;
+using DataAccess.Models;
+
+namespace LofovaChyse.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        [Authorize]
+        public ActionResult Index()
+        {
+            KnihovnaUserDao knihovnaUserDao = new KnihovnaUserDao();
+            KnihovnaUser knihovnaUser = knihovnaUserDao.GetByLogin(User.Identity.Name);
+
+
+            return View(knihovnaUser);
+        }
+    }
+}
