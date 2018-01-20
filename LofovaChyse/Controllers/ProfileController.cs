07a1693f8ebbbe1c14162c62b9adcaa322c1263e
Matěj Štágl <stagl.matej@centrum.cz>
2018-01-20 12:09:34 +0100

    Přidány uživatelské zdi

--- LofovaChyse/Controllers/ProfileController.cs
+++ LofovaChyse/Controllers/ProfileController.cs
@@ -38,12 +38,65 @@ namespace LofovaChyse.Controllers
         {
             KnihovnaUserDao knihovnaUserDao = new KnihovnaUserDao();
             KnihovnaUser knihovnaUser = knihovnaUserDao.GetByLogin(User.Identity.Name);
-
+           
             knihovnaUser.WelcomeText = welcomeText;
 
             knihovnaUserDao.Update(knihovnaUser);
 
             return RedirectToAction("Index", new {id = knihovnaUser.Id});
+        }
+
+        [Authorize]
+        public ActionResult ViewWall(int id)
+        {
+            KnihovnaUserDao knihovnaUserDao = new KnihovnaUserDao();
+            KnihovnaUser knihovnaUser = knihovnaUserDao.GetbyId(id);
+
+            bool v = false;
+
+            if (knihovnaUser.Name == knihovnaUserDao.GetByLogin(User.Identity.Name).Name)
+            {
+                v = true;
+            }
+            else
+            {
+                v = false;
+            }
+
+            ViewBag.Owner = v;
+            ViewBag.OwnerName = knihovnaUser.Name;
+            ViewBag.OwnerObject = knihovnaUser;
+            ViewBag.OwnerID = knihovnaUser.Id;
+
+            KnihovnaZedDao zedDao = new KnihovnaZedDao();
+
+            IList<KnihovnaZed> knihovnaZed = zedDao.GetUserWall(id);
+
+            return View(knihovnaZed);
+        }
+
+        [Authorize]
+        public ActionResult AddToWall(KnihovnaZed zed, int userId)
+        {
+            if (ModelState.IsValid)
+            {
+                KnihovnaZedDao dao = new KnihovnaZedDao();
+
+                KnihovnaZed novaZed = new KnihovnaZed();
+                novaZed.Id = zed.Id;
+                novaZed.PostDate = zed.PostDate;
+                novaZed.PostOwner = new KnihovnaUserDao().GetByLogin(User.Identity.Name);
+                novaZed.PostText = zed.PostText;
+                novaZed.Version = 1;
+
+                dao.Create(novaZed);
+            }
+
+            KnihovnaZedDao zedDao = new KnihovnaZedDao();
+
+            IList<KnihovnaZed> knihovnaZed = zedDao.GetUserWall(userId);
+
+            return RedirectToAction("ViewWall", knihovnaZed);
         }
     }
 }
