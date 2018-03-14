e2a3ad1bb4d80df2b809e05379cab0352e65355d
Matěj Štágl <stagl.matej@centrum.cz>
2018-03-15 00:05:53 +0100

    [sql] - Uživatelé mohou mít více rolí

---
+++ DataAccess/Dao/KnihovnaUserRoleDao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Models;
+using NHibernate.Criterion;
+
+namespace DataAccess.Dao
+{
+    public class KnihovnaUserRoleDao : DaoBase<KnihovnaUserRole>
+    {
+        public IList<KnihovnaUserRole> GetUserRoles(int userId)
+        {
+            return session.CreateCriteria<KnihovnaUserRole>().Add(Restrictions.Like("UserId", userId)).List<KnihovnaUserRole>();
+        }
+    }
+}
---
+++ DataAccess/Models/KnihovnaUserRole.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Interface;
+
+namespace DataAccess.Models
+{
+    public class KnihovnaUserRole : IEntity
+    {
+        public virtual int Id { get; set; }
+        public virtual int UserId { get; set; }
+        public virtual int RoleId { get; set; }
+        public virtual DateTime DateFrom { get; set; }
+        public virtual DateTime DateTo { get; set; }
+        public virtual int Data { get; set; }
+    }
+}
