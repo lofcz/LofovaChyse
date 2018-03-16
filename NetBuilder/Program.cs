1933b8909fb5577ba960e109721713a5a651f227
Cloudy <crosska111@gmail.com>
2018-03-16 17:19:04 +0100

    NetBuilder

---
+++ NetBuilder/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace NetBuilder
+{
+    class Program
+    {
+        private static String SolutionPath = "../../../../";
+        private static String ProjectPath = SolutionPath + "LofovaChyse/";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LofovaChyse NetBuilder\nChecking all needed things..");
+            Check();
+            Console.WriteLine("Checking complete.\nDo you want to analyze changes in dump db?\n\tY = Yes, apply changes\n\tO = Yes, but only analyze\n\tN = No");
+            Console.Write("> ");
+            Console.ReadLine();
+        }
+
+        private static void Check()
+        {
+            Console.WriteLine("Current path: " + Directory.GetCurrentDirectory());
+            if (!File.Exists(ProjectPath + "hibernate.cfg.xml"))
+            {
+                throw new Exception("NetBuilder coundn't find hibernate configuration file.");
+            }
+        }
+    }
+}
