3b3d0877cd173db91855694dd5782d2d6df9c90e
Matěj Štágl <stagl.matej@centrum.cz>
2018-03-20 20:42:14 +0100

    Přidány tabulky pro vouchery

---
+++ DataAccess/Models/KnihovnaVouchery.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Interface;
+
+namespace DataAccess.Models
+{
+     public class KnihovnaVouchery : IEntity
+    {
+        public virtual int Id { get; set; }
+        public virtual string Code { get; set; }
+        public virtual bool Used { get; set; }
+        public virtual int UsedId { get; set; }
+        public virtual DateTime UsedDate { get; set; }
+        public virtual int Type { get; set; }
+    }
+}
---
+++ DataAccess/Models/KnihovnaVoucheryArchetyp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Interface;
+
+namespace DataAccess.Models
+{
+    public class KnihovnaVoucheryArchetyp : IEntity
+    {
+        public virtual int Id { get; set; }
+        public virtual int Type { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Description { get; set; }
+    }
+}
